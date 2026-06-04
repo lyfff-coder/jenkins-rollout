ab1fe70d6105942ae591185ffbf41e1cc8f234ad
lyfff-coder <2390166240@qq.com>
2026-06-04 11:02:47 +0800

    Update rollout.sh

--- rollout.sh
+++ rollout.sh
@@ -1,1 +1,7 @@
-kubectl rollout undo deployment jenkins-demo  --to-revision=<version> -n <namespace>
+# kubectl rollout undo deployment jenkins-demo  --to-revision=<version> -n <namespace>
+
+INFO=$(kubectl rollout history deployment.apps/jenkins-demo -n <namespace> |awk '!/deployment/&&!/REVISION/&&NF>0{print $1","substr($0,index($0,"备注:"))}' | head -1)
+# 切割版本号
+REV=$(echo ${INFO} | cut -d',' -f1)
+# 回滚命令：-n 只传1个命名空间
+kubectl rollout undo deployment jenkins-demo --to-revision=${REV} -n <namespace>
