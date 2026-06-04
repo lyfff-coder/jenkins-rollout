5cdc5d6d2dcd1999eb8bcb07fce6af2fa3365fa4
lyfff-coder <2390166240@qq.com>
2026-06-04 16:20:52 +0800

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,6 +1,1 @@
-#kubectl rollout history deployment.apps/jenkins-demo -n production |grep -v "deployment" |grep -v "REVISION" |awk '$0!=""{print $1","substr($0,index($0,"备注:"))}' > version.csv
-
-NS=$1
-# 先清空旧文件
-rm -f version.csv
-kubectl rollout history deployment.apps/jenkins-demo -n ${NS} |grep -v "deployment" |grep -v "REVISION" |awk '$0!=""{print $1","substr($0,index($0,"备注:"))}' > version.csv
+#kubectl rollout history deployment.apps/jenkins-demo -n <namespace> |grep -v "deployment" |grep -v "REVISION" |awk '$0!=""{print $1","substr($0,index($0,"备注:"))}' > version.csv
