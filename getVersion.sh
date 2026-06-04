68f3ce389f6b9b7667b02e223f753ff44a89693b
lyfff-coder <2390166240@qq.com>
2026-06-04 16:21:14 +0800

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,1 +1,1 @@
-#kubectl rollout history deployment.apps/jenkins-demo -n <namespace> |grep -v "deployment" |grep -v "REVISION" |awk '$0!=""{print $1","substr($0,index($0,"备注:"))}' > version.csv
+kubectl rollout history deployment.apps/jenkins-demo -n <namespace> |grep -v "deployment" |grep -v "REVISION" |awk '$0!=""{print $1","substr($0,index($0,"备注:"))}' > version.csv
